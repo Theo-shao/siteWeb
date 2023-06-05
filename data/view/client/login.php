b0c252966c6a35b0d40a88263601cb4317d2c7de
root <lilan101@gmail.com>
2023-06-05 17:09:36 +0200

    shqo

--- data/view/client/login.php
+++ data/view/client/login.php
@@ -1,125 +1,222 @@
-<!DOCTYPE HTML> 
+<!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>菜鸟教程(runoob.com)</title>
-<style>
-.error {color: #FF0000;}
-</style>
-</head>
-<body> 
-
-<?php
-// 定义变量并默认设置为空值
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST")
-{
-    if (empty($_POST["name"]))
-    {
-        $nameErr = "名字是必需的";
-    }
-    else
-    {
-        $name = test_input($_POST["name"]);
-        // 检测名字是否只包含字母跟空格
-        if (!preg_match("/^[a-zA-Z ]*$/",$name))
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <!-- keywords -->
+	    <meta name="keywords" content="Auberge de jeunesse,Voyage,Reservation en ligne,Reservation d'hotel,Pratique,Location">
+	    <!-- description -->
+	    <meta name="description" content="SCI-Europe bénéficie d'une situation géographique supérieure en France, d'un transport pratique, d'un environnement confortable, d'un service attentionné et fournit des informations touristiques locales détaillées. Choisissez l'auberge de jeunesse SCI et profitez pleinement de votre voyage à Paris ! !">
+	    <!-- petit icon -->
+	    <link rel="shortcut icon" href="" />
+        <title>SCI-EUROPE/Activité</title>
+        <link rel="stylesheet" href="../../../css/app.css" />
+    </head>
+    <body style="filter: gray;">
+    <?php
+    $password = $email = "";
+    $emailErr = $passwordErr = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {   
+        if (empty($_POST["email"]))
         {
-            $nameErr = "只允许字母和空格"; 
+            $emailErr = "E-mail est obligatoire";
+        }
+        else
+        {
+            $email = test_input($_POST["email"]);
+            // 检测邮箱是否合法
+            if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email))
+            {
+                $emailErr = "Format de boîte aux lettres illégal"; 
+            }
         }
     }
-    
-    if (empty($_POST["email"]))
-    {
-      $emailErr = "邮箱是必需的";
-    }
-    else
-    {
-        $email = test_input($_POST["email"]);
-        // 检测邮箱是否合法
-        if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email))
-        {
-            $emailErr = "非法邮箱格式"; 
-        }
-    }
-    
-    if (empty($_POST["website"]))
-    {
-        $website = "";
-    }
-    else
-    {
-        $website = test_input($_POST["website"]);
-        // 检测 URL 地址是否合法
-        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website))
-        {
-            $websiteErr = "非法的 URL 的地址"; 
-        }
-    }
-    
-    if (empty($_POST["comment"]))
-    {
-        $comment = "";
-    }
-    else
-    {
-        $comment = test_input($_POST["comment"]);
-    }
-    
-    if (empty($_POST["gender"]))
-    {
-        $genderErr = "性别是必需的";
-    }
-    else
-    {
-        $gender = test_input($_POST["gender"]);
-    }
-}
 
-function test_input($data)
-{
+    function test_input($data)
+    {
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
-}
-?>
-
-<h2>PHP 表单验证实例</h2>
-<p><span class="error">* 必需字段。</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
-   名字: <input type="text" name="name" value="<?php echo $name;?>">
-   <span class="error">* <?php echo $nameErr;?></span>
-   <br><br>
-   E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-   <span class="error">* <?php echo $emailErr;?></span>
-   <br><br>
-   网址: <input type="text" name="website" value="<?php echo $website;?>">
-   <span class="error"><?php echo $websiteErr;?></span>
-   <br><br>
-   备注: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-   <br><br>
-   性别:
-   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?>  value="female">女
-   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?>  value="male">男
-   <span class="error">* <?php echo $genderErr;?></span>
-   <br><br>
-   <input type="submit" name="submit" value="Submit"> 
-</form>
-
-<?php
+    }
+    ?>
+        <div class="login-and-registration-con login">
+            <div class="login-and-registration">
+                <h1>
+                    <a href="./"></a>
+                </h1>
+                <div class="login-and-registration-form">
+                    <form class="login-form" action="" method="post" id="form">
+                        <h2>
+                            <span>Bienvenu à tous
+                                <font style="vertical-align:super;font-size:50%; ">®</font>
+                            </span>
+                        </h2>
+        
+                        <dl class="login-and-registration-dl">
+                            <dd>
+                                <label class="login-and-registration-icon1"></label><input type="text" placeholder="E-mail" name="username"><span class="error"><?php echo $emailErr;?></span>
+                            </dd>       
+                            <dd>
+                                <label class="login-and-registration-icon2"></label><input type="password" placeholder="mot de passe" name="password"><span class="error"><?php echo $passwordErr;?></span>
+                            </dd>
+                        </dl>
+                        
+                        <div class="self-and-forget clearfix">
+                            <span class="checkbox_item fl">
+                                <input type="checkbox" name="remember" value="1">
+                                <label class="check_label">
+                                    <i class="checkbox_icon"></i>
+                                    <span class="checkbox_text">Automatique</span> 
+                                </label>       
+                            </span>
+                            <!-- <span class="forget fr">oublier le mot de passe ? <a href="/web-index-getpassword1">点击重置&gt;&gt;</a></span> -->
+                        </div>
+                        <!-- <a href="javascript:$('#form').submit()" class="login-btn">登录</a> -->
+                        <input type="submit" name="submit" class="login-btn" value="Submit"> 
+                    </form>    
+                    <?php
 echo "<h2>您输入的内容是:</h2>";
 echo $name;
 echo "<br>";
 echo $email;
 echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-?>
-
-</body>
+echo $emailErr;
+?>         
+                </div>
+            </div>
+        </div>
+        
+        
+        
+        <script src="../../../js/jquery.1.12.1.min.js"></script>
+        <script src="../../../js/checkbox.js"></script>
+        
+        <script type="text/javascript">
+            $(".check_label").checkbox();
+            jQuery(document).ready(function() {
+                var autoLogin = getCookie('autoLogin');
+                if ( autoLogin ){
+                    autoLogin = decodeURI(autoLogin);
+                    var arr = autoLogin.split("|");
+                    var u = arr[0];
+                    var p = arr[1];
+                    if( u && p ) {
+                        $.post("web-index-autoLoginCheck",{username:u,password:p},function(msg){
+                            if ( 1==msg ) {
+                                location.href = "web-index-index"
+                            }
+                        })
+                    }
+                }else{
+                    var sid = setInterval(function(){
+                        $.get("web-index-isLogin",function(m){
+                            if(m == 0){
+                                //clearInterval(sid);
+                                location.href = "web-index-index";
+                                return;
+                            }
+                            if(m==1){
+                                location.href = "web-index-bind?t=qq";
+                            }
+                            if(m==2){
+                                location.href = "web-index-bind?t=wx";
+                            }
+                        })
+                    }, 2000)
+                }
+            });
+        
+        
+        
+            function getCookie(cname) {
+        
+                var name = cname + "=";
+        
+                var ca = document.cookie.split(';');
+        
+                for(var i=0; i<ca.length; i++) {
+        
+                    var c = ca[i];
+        
+                    while (c.charAt(0)==' ') c = c.substring(1);
+        
+                    if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+        
+                }
+        
+                return "";
+        
+            }
+        
+        </script>
+        
+        
+        
+        <script type="text/javascript">
+        
+            var childWindow;
+        
+            function toQzoneLogin() {
+        
+                childWindow = window.open("web-index-qqlogin","TencentLogin","width=450,height=320,menubar=0,scrollbars=1, resizable=1,status=1,titlebar=0,toolbar=0,location=1");
+        
+            }
+        
+        
+        
+            function closeChildWindow() {
+        
+                childWindow.close();
+        
+            }
+        
+        </script>
+        
+        
+        
+        <script type="text/javascript">
+        
+            var childWindow;
+        
+            var path = "https://open.weixin.qq.com/connect/qrconnect";
+        
+            var appid = 'wx387fcce1da633cf9';
+        
+            var redirect_uri = "http://www.yhachina.com/web-index-wxloginCallback";
+        
+        
+        
+            var path = "https://open.weixin.qq.com/connect/qrconnect?appid=" + appid + "&redirect_uri=" + encodeURI(redirect_uri);
+        
+            path += "&response_type=code&scope=snsapi_login";
+        
+        
+        
+            function toWeixinLogin(){
+        
+                childWindow = window.open(
+        
+                        path,
+        
+                        "TencentLogin",
+        
+                        "width=450,height=520,menubar=0,scrollbars=1, resizable=1,status=1,titlebar=0,toolbar=0,location=1"
+        
+                );
+        
+            }
+        
+            function closeChildWindow(){
+        
+                childWindow.close();
+        
+            }
+        
+        </script>
+        
+        
+        
+    </body>
 </html>
