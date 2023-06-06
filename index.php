e3816771a43a494fc78513d1cccefb5aba98f188
root <lilan101@gmail.com>
2023-06-06 17:04:46 +0200

    shqo

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
 			<div class="top-user-panel">          
 				<a href="http://localhost/site/siteWeb/data/view/client/gestion.html" class="user-login">Gestion de commande</a>       
 				|                           
-				<a href="http://localhost/site/siteWeb/data/view/client/inscription.html" class="user-login"><img src="images/market/user-icon.png">Inscription</a>  
+				<a href="http://localhost/site/siteWeb/data/view/client/inscription.php" class="user-login"><img src="images/market/user-icon.png">Inscription</a>  
 				| 
 				<a href="http://localhost/site/siteWeb/data/view/client/login.php" class="user-login">Connexion</a>                                 
 				<div class="lang">                    
