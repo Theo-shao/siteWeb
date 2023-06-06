5f18b8d14bf6516a9375b216d8bcb9ab6980d815
root <lilan101@gmail.com>
2023-06-06 15:30:31 +0200

    shqo

--- data/view/client/login.php
+++ data/view/client/login.php
@@ -104,10 +104,10 @@
         }
         else{
             $password = test_input($_POST["password"]);
-            $sql = "SELECT * FROM usertable WHERE email=$email and pass=$password";
+            $sql = "SELECT * FROM usertable u WHERE u.email=$email and u.pass=$password";
             $result = $conn->query($sql);
               // 检查查询是否成功
-            if ($result == false) {
+            if ($result === false) {
                 echo "查询失败: " . $conn->error.$email.$password;
             } else {
     // 获取结果集的行数
