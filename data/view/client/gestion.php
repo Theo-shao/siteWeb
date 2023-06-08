ff3ab01db46650834aabb1dce55e7e703d5b43d7
root <lilan101@gmail.com>
2023-06-08 12:24:11 +0200

    shao

--- data/view/client/gestion.php
+++ data/view/client/gestion.php
@@ -149,11 +149,21 @@
                             if ($result === false) {
                                 echo $conn->error;
                             } else {
-                                if ($result->num_rows > 0) {
-                                    while ($row = $result->fetch_assoc()) {
-                                      echo "ID: " . $row["id"] . ", Name: " . $row["email"] . "<br>";
-                                    }
-                                  }
+                            //     if ($result->num_rows > 0) {
+                            //         while ($row = $result->fetch_assoc()) {
+                            //           echo "ID: " . $row["id"] . ", Name: " . $row["email"] . "<br>";
+                            //         }
+                            //       }
+                            // }
+                            
+                                $numRows = $result->num_rows;
+                                if($numRows==1){
+                                    echo "Connexion réussie";
+                                }
+                                else{
+                                    echo "Mot de pass n'est pas correct";
+                                }
+                                $result->free_result();
                             }
                         }
                         else if($Meth=="Télephone"){
