6ba7339f0167880d4e8f8c11a86a76ce2aa6d90c
root <lilan101@gmail.com>
2023-06-12 12:32:05 +0200

    shao

--- data/view/info/reserver.php
+++ data/view/info/reserver.php
@@ -115,9 +115,10 @@
             </div>
         </div>
         <?php
+                $stime = test_input($_POST["stime"]);
+                $etime = test_input($_POST["etime"]);
                 if ($_SERVER["REQUEST_METHOD"] == "POST"){
-                    $stime = test_input($_POST["stime"]);
-                    $etime = test_input($_POST["etime"]);
+                    
                     $sql = "SELECT * from chambres where num NOT IN (SELECT ch.num FROM commend c,chambres ch WHERE ch.num=c.num_chambre and '$stime'<c.datefin ) ;";
                     $result = $conn->query($sql);
         ?>
